334884d2ec724cc1af472e85282a0370a2192073
noumandatu <noumankhan619.915@gmail.com>
2024-07-04 16:24:04 +0500

    first commit

---
+++ src/utils/theme/typo.ts
@@ -0,0 +1,39 @@
+import { styled } from "@mui/system";
+import Typography from "@mui/material/Typography";
+
+export const Mainheading = styled(Typography)({
+  fontFamily: "Urbanist",
+  fontWeight: 700,
+  fontSize: "24px",
+  lineHeight: "31.2px",
+});
+
+export const Heading = styled(Typography)({
+  fontFamily: "Plus Jakarta Sans",
+  fontWeight: 600,
+  fontSize: "15.91px",
+  lineHeight: "26.87px",
+});
+
+export const Font = styled(Typography)({
+  fontFamily: "Urbanist",
+  fontWeight: 400,
+  fontSize: "15.91px",
+  lineHeight: "24px",
+});
+
+export const Label = styled(Typography)({
+  fontFamily: "Urbanist",
+  fontWeight: 500,
+  fontSize: "16px",
+  lineHeight: "16px",
+  color: "#9393AA",
+});
+
+export const Caption = styled(Typography)({
+  fontFamily: "Urbanist",
+  fontWeight: 400,
+  fontSize: "14px",
+  lineHeight: "22.4px",
+  color: "#9393AA",
+});
